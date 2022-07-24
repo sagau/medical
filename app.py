999d2fd0ef4a6fd62221ba1be3fccef88625f100
sagau <kkunkel@gmail.com>
2022-07-23 23:06:17 -0500

    plotly update requirements, demo data-chart

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import joblib
 import pandas as pd
 import streamlit as st
 from PIL import Image
+import plotly.express as px
 
 model = joblib.load('etc_trained_model.job')
 
@@ -45,6 +46,7 @@ with st.expander("See explanation"):
         You can also upload your own csv file with multiple entries to make predictions for several articles at once and then download the results.
      """)
 file = st.file_uploader('Upload CSV File')
+demo = st.button(label='Demo Data')
 
 with st.expander("CSV Template (Copy & Paste)"):
     st.write('Copy the code below into an empty csv file...')
@@ -53,6 +55,40 @@ with st.expander("CSV Template (Copy & Paste)"):
     ,title,authors,item_text
     0,your title,the authors,"The document text." """
     )
+
+if demo:
+    with st.spinner('Wait for it, making your predictions...'):
+            df = pd.read_csv('ExampleData.csv', index_col=0)
+            df.reset_index(drop=True, inplace=True)
+            df.insert(0, 'category_prediction', model.predict(df))
+            st.dataframe(df)
+            @st.cache
+            def convert_df(df):
+                return df.to_csv().encode('utf-8')
+
+            fig = px.bar(        
+            df['category_prediction'].value_counts(),
+            title = "Predicted Categories",
+            orientation = 'h' #Optional Parameter
+            )
+            st.plotly_chart(fig)
+
+
+            csv = convert_df(df)
+
+            col1, col2, col3, col4 = st.columns(4)
+
+            with col1:
+                st.download_button(
+                    label="Download CSV File",
+                    data=csv,
+                    file_name='NLP-Predictions.csv',
+                    mime='text/csv',
+                )
+            with col2:
+                clear = st.button('Clear')
+                if clear:
+                    demo = False
 
 if file:
     try:
@@ -64,7 +100,12 @@ if file:
             @st.cache
             def convert_df(df):
                 return df.to_csv().encode('utf-8')
-
+            fig = px.bar(        
+            df['category_prediction'].value_counts(),
+            title = "Predicted Categories",
+            orientation = 'h' #Optional Parameter
+            )
+            st.plotly_chart(fig)
             csv = convert_df(df)
 
             st.download_button(
@@ -73,11 +114,12 @@ if file:
                 file_name='NLP-Predictions.csv',
                 mime='text/csv',
             )
+            
     except:
         st.error('Please make sure you are using the right file format. Use the template if necessary.')
 
 
-if not file:
+if not demo and not file:
     st.write('Please enter the article information below:')
     with st.form('article_input', clear_on_submit=True):
         title = st.text_input(label='Title')
